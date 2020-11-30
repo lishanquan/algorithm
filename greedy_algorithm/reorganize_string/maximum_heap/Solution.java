25b0e08712fff8dcfade1fe166b393572b652f5a
lishanquan <991887313@qq.com>
2020-11-30 17:05:54 +0800

    first commit

---
+++ greedy_algorithm/reorganize_string/maximum_heap/Solution.java
@@ -0,0 +1,59 @@
+package greedy_algorithm.reorganize_string.maximum_heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(reorganizeString("aaababaacbb"));
+    }
+
+    public static String reorganizeString(String S) {
+        if (S.length() < 2) {
+            return S;
+        }
+        int[] counts = new int[26];
+        int maxCount = 0;
+        int len = S.length();
+        for (int i = 0; i < len; i++) {
+            char c = S.charAt(i);
+            counts[c - 'a']++;
+            maxCount = Math.max(maxCount, counts[c - 'a']);
+        }
+        if (maxCount > (len + 1) / 2) {
+            return "";
+        }
+        PriorityQueue<Character> queue = new PriorityQueue<Character>(new Comparator<Character>() {
+            public int compare(Character letter1, Character letter2) {
+                return counts[letter2 - 'a'] - counts[letter1 - 'a'];
+            }
+        });
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (counts[c - 'a'] > 0) {
+                queue.offer(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (queue.size() > 1) {
+            char letter1 = queue.poll();
+            char letter2 = queue.poll();
+            sb.append(letter1);
+            sb.append(letter2);
+            int index1 = letter1 - 'a', index2 = letter2 - 'a';
+            counts[index1]--;
+            counts[index2]--;
+            if (counts[index1] > 0) {
+                queue.offer(letter1);
+            }
+            if (counts[index2] > 0) {
+                queue.offer(letter2);
+            }
+        }
+        if (queue.size() > 0) {
+            sb.append(queue.poll());
+        }
+        return sb.toString();
+    }
+
+}
